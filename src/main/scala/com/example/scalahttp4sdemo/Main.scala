51b103a713c39566395dc0309a49d4cbf53300aa
JinxiaRebecca <18502908857@163.com>
2022-05-25 15:12:19 +0800

    http4s quick start

---
+++ src/main/scala/com/example/scalahttp4sdemo/Main.scala
@@ -0,0 +1,8 @@
+package com.example.scalahttp4sdemo
+
+import cats.effect.{ExitCode, IO, IOApp}
+
+object Main extends IOApp {
+  def run(args: List[String]) =
+    Scalahttp4sdemoServer.stream[IO].compile.drain.as(ExitCode.Success)
+}
