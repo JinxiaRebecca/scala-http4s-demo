76a05921701bbde7e355875749de5b9fb57f00d4
JinxiaRebecca <18502908857@163.com>
2022-05-30 17:35:17 +0800

    add CustomerDao

---
+++ src/main/scala/com/example/scalahttp4sdemo/dao/CustomerDao.scala
@@ -0,0 +1,18 @@
+package com.example.scalahttp4sdemo.dao
+
+import com.example.scalahttp4sdemo.service.Customer
+
+import java.time.LocalDate
+
+class CustomerDao {
+  val now: LocalDate = LocalDate.now()
+  val customers: List[Customer] = List(
+    Customer(1, "Lily", 1, now.minusDays(7)),
+    Customer(2, "Coco", 2, now.minusDays(5)),
+    Customer(3, "Nico", 3, now.minusDays(6)),
+  )
+
+  def fetchAllCustomers(): List[Customer] = customers
+  def fetchCustomerById(customerId: Int): Customer = customers.filter(_.id == customerId).head
+
+}
