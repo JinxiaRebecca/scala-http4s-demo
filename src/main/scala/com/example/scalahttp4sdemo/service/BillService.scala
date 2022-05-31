c7ed1103af71eda32d174e990f2d0556fcfca01f
JinxiaRebecca <18502908857@163.com>
2022-05-31 09:32:07 +0800

    add method for calculating bill per customer

--- src/main/scala/com/example/scalahttp4sdemo/service/BillService.scala
+++ src/main/scala/com/example/scalahttp4sdemo/service/BillService.scala
@@ -1,5 +1,6 @@
 package com.example.scalahttp4sdemo.service
 
+import com.example.scalahttp4sdemo.Utils
 import com.example.scalahttp4sdemo.dao.BillDao
 
 import java.time.LocalDate
@@ -12,17 +13,34 @@ case class Bill(
                  phoneUse: Int,
                  smsUse: Int,
                  consumptionCost: BigDecimal,
-                 billDate: LocalDate,
+                 billStartDate: LocalDate,
+                 billEndDate: LocalDate,
                  status: String)
 case class UsageResponse(
                           var phoneUseLeft: Int,
                           var smsUseLeft: Int,
                         )
 
-class BillService(billDao: BillDao) {
+class BillService(billDao: BillDao, usageService: UsageService, packageService: PackageService) {
   implicit val localDateOrdering: Ordering[LocalDate] = _ compareTo _
 
-  def getLatestBillDateByCustomerId(customerId: Int): LocalDate = billDao.getBillsByCustomerId(customerId).map(_.billDate).max
+  def getLatestBillDateByCustomerId(customerId: Int): LocalDate = billDao.getBillsByCustomerId(customerId).map(_.billEndDate).max
+
+  def calculateQueriedBillForSpecificCustomer(customer: Customer, queryDate: LocalDate): Bill = {
+    val subscribedDate = customer.subscribedDate
+    val currentBillStartDate = Utils.getRequiredBillPeriodStartDate(subscribedDate, queryDate)
+    val currentBillEndDate = Utils.getRequiredBillPeriodEndDate(subscribedDate, queryDate)
+    val phoneUsed = usageService.calculatePhoneUsagesForSpecificPeriodByCustomerId(customer.id, currentBillStartDate, currentBillEndDate)
+    val smsUsed = usageService.calculateSmsUsagesForSpecificPeriodByCustomerId(customer.id, currentBillStartDate, currentBillEndDate)
+    val packages = packageService.fetchPackageByPackageId(customer.packageId)
+    val phoneUseLeft = packages.phoneLimitation - phoneUsed
+    val smsUseLeft = packages.phoneLimitation - smsUsed
+    val exPhoneFee = if (phoneUseLeft < 0) Math.abs(phoneUseLeft) * packages.exPhoneFee else BigDecimal.valueOf(0)
+    val exSmsFee = if(smsUseLeft < 0) Math.abs(smsUseLeft) * packages.exSmsFee else BigDecimal.valueOf(0)
+    val totalCost = packages.subscriptionFee + exSmsFee + exPhoneFee
+    Bill(scala.util.Random.nextInt(Integer.MAX_VALUE), customer.id, customer.name, packages.name, phoneUsed, smsUsed, totalCost,
+      currentBillStartDate, currentBillEndDate, "pendingPayment")
+  }
 
 
 }
--- src/test/scala/com/example/scalahttp4sdemo/BillServiceSpec.scala
+++ src/test/scala/com/example/scalahttp4sdemo/BillServiceSpec.scala
@@ -1,7 +1,7 @@
 package com.example.scalahttp4sdemo
 
 import com.example.scalahttp4sdemo.dao.BillDao
-import com.example.scalahttp4sdemo.service.{Bill, BillService}
+import com.example.scalahttp4sdemo.service.{Bill, BillService, PackageService, UsageService}
 import org.mockito.Mockito.when
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
@@ -11,12 +11,14 @@ import java.time.LocalDate
 
 class BillServiceSpec extends AnyWordSpec with Matchers with MockitoSugar {
   val mockedBillDao: BillDao= mock[BillDao]
-  val billService: BillService = new BillService(mockedBillDao)
+  val mockedUsageService: UsageService = mock[UsageService]
+  val mockedPackageService: PackageService = mock[PackageService]
+  val billService: BillService = new BillService(mockedBillDao, mockedUsageService, mockedPackageService)
   val now: LocalDate = LocalDate.now()
   val mockedBills: List[Bill] = List(
-    Bill(1, 1, "Lily", "Starter", 0, 0, 38, now.minusDays(7), "completed"),
-    Bill(2, 2, "Coco", "Standard", 0, 0, 58, now.minusDays(5), "completed"),
-    Bill(3, 3, "Nico", "Premier", 0, 0, 188, now.minusDays(6), "completed")
+    Bill(1, 1, "Lily", "Starter", 0, 0, 38, now.minusDays(7), now.minusDays(7), "completed"),
+    Bill(2, 2, "Coco", "Standard", 0, 0, 58, now.minusDays(5), now.minusDays(5), "completed"),
+    Bill(3, 3, "Nico", "Premier", 0, 0, 188, now.minusDays(6),now.minusDays(6), "completed")
   )
 
   "Bill service spec" should {
