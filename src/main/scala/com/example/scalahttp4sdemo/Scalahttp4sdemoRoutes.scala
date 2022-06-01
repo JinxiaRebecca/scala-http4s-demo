c18ee62b99c2682732c68ad45c21172d4b07dc80
JinxiaRebecca <18502908857@163.com>
2022-06-01 16:17:53 +0800

    move UsageRoutes to controller pacakage

--- src/main/scala/com/example/scalahttp4sdemo/Scalahttp4sdemoRoutes.scala
+++ src/main/scala/com/example/scalahttp4sdemo/Scalahttp4sdemoRoutes.scala
@@ -2,7 +2,7 @@ package com.example.scalahttp4sdemo
 
 import cats.effect.Sync
 import cats.implicits._
-import com.example.scalahttp4sdemo.service.{Bill, BillService, CustomerService, PackageService, UsageService}
+import com.example.scalahttp4sdemo.service.{Bill, BillService, CustomerService}
 import io.circe.generic.codec.DerivedAsObjectCodec.deriveCodec
 import org.http4s.{HttpRoutes, ParseFailure, QueryParamDecoder}
 import org.http4s.dsl.Http4sDsl
@@ -14,10 +14,7 @@ import java.time.format.DateTimeFormatter
 import scala.util.Try
 
 object Scalahttp4sdemoRoutes {
-  case class UsageResponse(
-                            var phoneUseLeft: Int,
-                            var smsUseLeft: Int,
-                          )
+
 
 
   def jokeRoutes[F[_] : Sync](J: Jokes[F]): HttpRoutes[F] = {
@@ -41,22 +38,6 @@ object Scalahttp4sdemoRoutes {
           greeting <- H.hello(HelloWorld.Name(name))
           resp <- Ok(greeting)
         } yield resp
-    }
-  }
-
-  def UsageRoutes[F[_] : Sync](customerService: CustomerService, usageService: UsageService, packageService: PackageService): HttpRoutes[F] = {
-    val dsl = new Http4sDsl[F] {}
-    import dsl._
-    HttpRoutes.of[F] {
-      case GET -> Root / "reserved-usage" / IntVar(customerId) =>
-        val customer = customerService.fetchCustomerByCustomerId(customerId)
-        val subscribedDate = customer.subscribedDate
-        val currentBillStartDate = Utils.getRequiredBillPeriodStartDate(subscribedDate, LocalDate.now())
-        val currentBillEndDate = Utils.getRequiredBillPeriodEndDate(subscribedDate, LocalDate.now())
-        val phoneUse = usageService.calculatePhoneUsagesForSpecificPeriodByCustomerId(customerId, currentBillStartDate, currentBillEndDate)
-        val smsUse = usageService.calculateSmsUsagesForSpecificPeriodByCustomerId(customerId, currentBillStartDate, currentBillEndDate)
-        val packages = packageService.fetchPackageByPackageId(customer.packageId)
-        Ok(UsageResponse(packages.phoneLimitation - phoneUse, packages.smsLimitation - smsUse))
     }
   }
 
